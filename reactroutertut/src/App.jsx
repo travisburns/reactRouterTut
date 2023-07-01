8c6ca8bec175ec9fd6e7722f5474d929c5495858
Travis <travisjb0@gmail.com>
2023-06-30 20:24:17 -0700

    Added navbar links tut and learned links

--- reactroutertut/src/App.jsx
+++ reactroutertut/src/App.jsx
@@ -3,12 +3,14 @@ import {Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Home } from './Components/Home'
 import { About } from './Components/About'
+import Navbar from './Components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
