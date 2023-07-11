67680c5bce450cddf527cf7a47809a3a6150d0b2
Travis <travisjb0@gmail.com>
2023-07-11 15:28:58 -0700

    Remaking and redoing tutorial

--- reactroutertut/src/App.jsx
+++ reactroutertut/src/App.jsx
@@ -1,44 +1,106 @@
-import { useState } from 'react'
-import {Routes, Route } from 'react-router-dom'
-import './App.css'
-import { Home } from './Components/Home'
+import { Routes, Route} from 'react-router-dom'
 import { About } from './Components/About'
-import Navbar from './Components/Navbar'
-import OrderSummary from './Components/OrderSummary'
-import NoMatch from './Components/NoMatch'
-import Products from './Components/Products'
-import NewProducts from './Components/NewProducts'
-import FeaturedProducts from './Components/FeaturedProducts'
-import Users from './Components/Users'
-import UserDetails from './Components/UserDetails'
-import Admin from './Components/Admin'
+import { Home } from './Components/Home'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='order-summary' element={ <OrderSummary />} />
-        <Route path='products' element={ <Products />}>
-          <Route index element={FeaturedProducts}/>
-            <Route path='featured' element={<FeaturedProducts />} />
-            <Route path='New' element={<NewProducts /> } />
-            
-            
-          </Route> 
-          <Route path='users' element={<Users />}>
-            <Route path=':userId' element={<UserDetails />} />
-            <Route path='admin' element={<Admin />} />
-          </Route>
-        <Route path='*' element={<NoMatch />} />
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+    
+    </Routes>
 
-      </Routes>
-    </>
-  )
+  );
 }
 
-export default App
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import {Routes, Route } from 'react-router-dom'
+// import './App.css'
+// import { Home } from './Components/Home'
+// import { About } from './Components/About'
+// import Navbar from './Components/Navbar'
+// import OrderSummary from './Components/OrderSummary'
+// import NoMatch from './Components/NoMatch'
+// import Products from './Components/Products'
+// import NewProducts from './Components/NewProducts'
+// import FeaturedProducts from './Components/FeaturedProducts'
+// import Users from './Components/Users'
+// import UserDetails from './Components/UserDetails'
+// import Admin from './Components/Admin'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     <Navbar />
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='about' element={<About />} />
+//         <Route path='order-summary' element={ <OrderSummary />} />
+//         <Route path='products' element={ <Products />}>
+//           <Route index element={FeaturedProducts}/>
+//             <Route path='featured' element={<FeaturedProducts />} />
+//             <Route path='New' element={<NewProducts /> } />
+            
+            
+//           </Route> 
+//           <Route path='users' element={<Users />}>
+//             <Route path=':userId' element={<UserDetails />} />
+//             <Route path='admin' element={<Admin />} />
+//           </Route>
+//         <Route path='*' element={<NoMatch />} />
+
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default App
--- reactroutertut/src/Components/Home.jsx
+++ reactroutertut/src/Components/Home.jsx
@@ -1,15 +1,11 @@
-import { useNavigate } from "react-router-dom"
+import React from 'react'
 
-export const Home = () => {
-
-    const navigate = useNavigate()
-    return (
-    <>
-    <div> Home</div>
-    <button onClick={() => navigate('order-summary', {replace: true })}>Place Order</button>
-    </>
-    )
+const Home = () => {
+  return (
+    <div>
+      Home Page
+    </div>
+  )
 }
 
-
-export default Home 
+export default Home
