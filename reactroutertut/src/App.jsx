c60d195dd48af5513e996251ae4605cb595d21e8
Travis <travisjb0@gmail.com>
2023-07-12 20:52:55 -0700

    Added a bit of refresher to react router

--- reactroutertut/src/App.jsx
+++ reactroutertut/src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route} from 'react-router-dom'
-import { About } from './Components/About'
+
 import { Home } from './Components/Home'
 
-
+import { About } from './Components/About'
+import Navbar from './Components/Navbar';
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/about' element={<About />}></Route>
+    <>
     
-    </Routes>
+    <Navbar />
+
+<Routes>
+   <Route path='/' element={<Home />} />
+         <Route path='about' element={<About />} />
+</Routes>
+    
+    </>
+  
 
   );
 }
--- reactroutertut/src/Components/Navbar.jsx
+++ reactroutertut/src/Components/Navbar.jsx
@@ -1,23 +1,15 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+const navLinkStyles = () => {
 
-const navLinkStyles = ({ isActive }) => {
-    return {
-        fontWeight: isActive ? 'bold' : 'normal',
-        textDecoration: isActive ? 'none' : 'underline',
-    }
 }
 
 
-
   return (
-    <nav className='primary-nav'>
-        <NavLink style={navLinkStyles} to='/'> Home</NavLink>
-        <NavLink style={navLinkStyles} to='/about'> About</NavLink>
-        <NavLink style={navLinkStyles} to='/products'> products</NavLink>
+    <nav>
+      <NavLink style={navLinkStyles} to="/">Home</NavLink>
+      <NavLink style={navLinkStyles} to="/about">about</NavLink>
     </nav>
   )
 }
